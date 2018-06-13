84a4b6311a24efacb8ef01db34d9a44fc9dc35a8
crestrepo12 <CarolinaRestrepo@ac.c4q.nyc>
2018-06-13 14:59:06 -0400

    added a new Project on my projects page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,15 +5,35 @@ class Projects extends Component {
     return (
       <div id="projects" className="content background-image">
         <h1>Projects </h1>
+
         <div id="projects-container">
+          <div className="project shadow">
+            <h2>GearUP</h2>
+            <p>
+              <a href="https://gearup-management.herokuapp.com/">Live</a> |{" "}
+              <a href="https://github.com/crestrepo12/GearUP">Repo</a>
+            </p>
+            <img
+              src="https://i.gyazo.com/eb0ccae7f7ee2b565be709b2e60fc514.png"
+              alt="https://gyazo.com/eb0ccae7f7ee2b565be709b2e60fc514"
+              width="1280"
+              className="project-img"
+            />
+            <p>React, CSS (Flexbox), Semantic UI React, Express, PostgreSQL</p>
+          </div>
+
           <div className="project shadow">
             <h2>Tutelage</h2>
             <p>
               <a href="https://tutelage-connect.herokuapp.com/">Live</a> |{" "}
               <a href="https://github.com/Acostill/Tutelage">Repo</a>
             </p>
-            <img src="/images/MentorPage.png" alt="tutelage search page" className="project-img" />
-            <p>React, CSS, HTML, Express, Postgres, BCrypt</p>
+            <img
+              src="/images/MentorPage.png"
+              alt="tutelage search page"
+              className="project-img"
+            />
+            <p>React, CSS, Express, PostgreSQL, BCrypt</p>
           </div>
           <div className="project shadow">
             <h2>[V]ACCESS NYC</h2>
@@ -21,15 +41,16 @@ class Projects extends Component {
               <a href="https://vaccessnyc.netlify.com/">Live</a> |{" "}
               <a href="https://github.com/davidyshin/VACCessNYC">Repo</a>
             </p>
-            <img src="/images/vaccess.png" alt="vaccess home search page" className="project-img" />
+            <img
+              src="/images/vaccess.png"
+              alt="vaccess home search page"
+              className="project-img"
+            />
             <p>
-              React, CSS, HTML, Express, Postgres, BCrypt, React Google Maps,
-              NYC Open Data
+              React, CSS, Express, PostgreSQL, BCrypt, React Google Maps, NYC
+              Open Data
             </p>
           </div>
-          {/* <div className="project"><a href=""><img src="" alt=""/></a></div>
-          <div className="project"><a href=""><img src="" alt=""/></a></div>
-          <div className="project"><a href=""><img src="" alt=""/></a></div> */}
         </div>
       </div>
     );
