919f617c1040c8e80351d0b697f929e545187f7d
crestrepo12 <CarolinaRestrepo@ac.c4q.nyc>
2019-02-11 23:18:02 -0500

    used reusable variable for resume link

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import FontAwesomeIcon from "@fortawesome/react-fontawesome";
 import brands from "@fortawesome/fontawesome-free-brands";
 import solid from "@fortawesome/fontawesome-free-solid";
+import resumeLink from "resumeLink";
 
 class Sidebar extends Component {
   render() {
@@ -21,7 +22,7 @@ class Sidebar extends Component {
           <a href="mailto:carolinarestrepo@ac.c4q.nyc">
           <FontAwesomeIcon icon={["fas", "envelope"]} size="3x"/></a>
           {/* Link for resume */}
-          <a href="https://drive.google.com/file/d/15fb93abPxallGYEMb83jvo522LuHblfv/view?usp=sharing">
+          <a href={resumeLink}>
           <FontAwesomeIcon icon={["fas", "file-alt"]} size="3x"/></a>
         </div>
         <p> Made by Carolina Restrepo </p>
