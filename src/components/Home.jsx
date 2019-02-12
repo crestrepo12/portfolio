15956424ca84adc148560b33eb7b46cb69694eb9
crestrepo12 <CarolinaRestrepo@ac.c4q.nyc>
2019-02-11 23:17:36 -0500

    used reusable variable for resume link

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React, { Component} from "react";
 import FontAwesomeIcon from "@fortawesome/react-fontawesome";
+import resumeLink from "resumeLink";
 
 class Home extends Component {
   render() {
@@ -8,7 +9,7 @@ class Home extends Component {
         <h1>Carolina Restrepo</h1>
         <h3>Full Stack Web Developer</h3>
 
-        <a href="https://drive.google.com/file/d/15fb93abPxallGYEMb83jvo522LuHblfv/view?usp=sharing" className="button">
+        <a href={resumeLink}>
             <h4>RESUME</h4>
             <FontAwesomeIcon icon={["fas", "file-alt"]} size="2x" />
         </a>
